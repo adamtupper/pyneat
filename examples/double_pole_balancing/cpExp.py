bc89fc6fc926be8b9508793ae6bb02bda7ac2958
Adam Tupper <adam.tupper@outlook.com>
2020-04-15 09:54:46 +1200

    Enable performing multiple runs.

--- examples/double_pole_balancing/cpExp.py
+++ examples/double_pole_balancing/cpExp.py
@@ -37,12 +37,6 @@ def evaluate_population(population, config):
 
 
 if __name__ == "__main__":
-
-    if not os.path.exists('results/checkpoints/'):
-        os.makedirs('results/checkpoints/')
-    if not os.path.exists('results/genomes/'):
-        os.makedirs('results/genomes/')
-
     config = CustomConfig(Genome, Reproduction, SpeciesSet, neat.DefaultStagnation, 'dpnv_config.ini')
 
     # change the number of inputs accordingly to the type
@@ -59,60 +53,67 @@ if __name__ == "__main__":
     # pop = population.Population()
     # pop.epoch(500, report=1, save_best=0)
 
-    population = Population(config)
+    # population = Population(config)
+    for r in range(config.num_runs):
+        if not os.path.exists(f'results/run-{r}/checkpoints/'):
+            os.makedirs(f'results/run-{r}/checkpoints/')
+        if not os.path.exists(f'results/run-{r}/genomes/'):
+            os.makedirs(f'results/run-{r}/genomes/')
 
-    # Add reporters
-    stats = neat.StatisticsReporter()
-    population.add_reporter(stats)
-    population.add_reporter(neat.StdOutReporter(True))
-    population.add_reporter(neat.Checkpointer(generation_interval=config.checkpoint_interval,
-                                              time_interval_seconds=None,
-                                              filename_prefix='results/' + 'checkpoints/' + 'neat-checkpoint-'))
+        population = Population(config)
 
-    # Set generation limit
-    max_generations = config.max_generations
-    generation = 0
-    while generation < max_generations:
-        # Draw genomes
-        for key, genome in population.population.items():
-            visualize.draw_net(genome, filename=f'results/genomes/genome_{key}')
+        # Add reporters
+        stats = neat.StatisticsReporter()
+        population.add_reporter(stats)
+        population.add_reporter(neat.StdOutReporter(True))
+        population.add_reporter(neat.Checkpointer(generation_interval=config.checkpoint_interval,
+                                                  time_interval_seconds=None,
+                                                  filename_prefix='results/' + f'run-{r}/' + 'checkpoints/' + 'neat-checkpoint-'))
 
-        batch_size = 1
-        best = population.run(fitness_function=evaluate_population, n=batch_size)
+        # Set generation limit
+        max_generations = config.max_generations
+        generation = 0
+        while generation < max_generations:
+            # # Draw genomes
+            # for key, genome in population.population.items():
+            #     visualize.draw_net(genome, filename=f'results/genomes/genome_{key}')
 
-        visualize.plot_stats(stats, ylog=False, view=False, filename='results/' + "fitness.svg")
+            batch_size = 1
+            best = population.run(fitness_function=evaluate_population, n=batch_size)
 
-        mfs = sum(stats.get_fitness_mean()[-5:]) / 5.0
-        print("Average mean fitness over last 5 generations: {0}".format(mfs))
+            visualize.plot_stats(stats, ylog=False, view=False, filename='results/' + f'run-{r}/' + "fitness.svg")
 
-        mfs = sum(stats.get_fitness_stat(min)[-5:]) / 5.0
-        print("Average min fitness over last 5 generations: {0}".format(mfs))
+            mfs = sum(stats.get_fitness_mean()[-5:]) / 5.0
+            print("Average mean fitness over last 5 generations: {0}".format(mfs))
 
-        # Save current best
-        # save_object(best, base_dir + f'solution_{generation}.pickle')
+            mfs = sum(stats.get_fitness_stat(min)[-5:]) / 5.0
+            print("Average min fitness over last 5 generations: {0}".format(mfs))
 
-        generation += batch_size
+            # Save current best
+            # save_object(best, base_dir + f'solution_{generation}.pickle')
 
-    # Save best genome and stats reporter
-    if best:
-        print('Saving best genome...')
-        save_object(best, 'results/' + 'solution.pickle')
-        save_object(stats, 'results/' + 'stats_reporter.pickle')
+            generation += batch_size
 
-    # visualize the best topology
-    #visualize.draw_net(winner) # best chromosome
-    # Plots the evolution of the best/average fitness
-    #visualize.plot_stats(pop.stats)
-    # Visualizes speciation
-    #visualize.plot_species(pop.species_log)
+        # Save best genome and stats reporter
+        if best:
+            print('Saving best genome...')
+            save_object(best, 'results/' + f'run-{r}/' + 'solution.pickle')
+            save_object(stats, 'results/' + f'run-{r}/' + 'stats_reporter.pickle')
 
-    print(f'Number of evaluations: {best.key}')
-    print(f'Winner score: {best.score}')
-    # from time import strftime
-    # date = strftime("%Y_%m_%d_%Hh%Mm%Ss")
-    # saves the winner
-    # file = open('winner_'+date, 'w')
-    # pickle.dump(winner, file)
-    # file.close()
-    pickle.dump(population, open('results/population.pickle', 'wb'))
-    #print winner
+        # visualize the best topology
+        #visualize.draw_net(winner) # best chromosome
+        # Plots the evolution of the best/average fitness
+        #visualize.plot_stats(pop.stats)
+        # Visualizes speciation
+        #visualize.plot_species(pop.species_log)
+
+        print(f'Number of evaluations: {best.key}')
+        print(f'Winner score: {best.score}')
+        # from time import strftime
+        # date = strftime("%Y_%m_%d_%Hh%Mm%Ss")
+        # saves the winner
+        # file = open('winner_'+date, 'w')
+        # pickle.dump(winner, file)
+        # file.close()
+        pickle.dump(population, open(f'results/run-{r}/final_population.pickle', 'wb'))
+        #print winner
