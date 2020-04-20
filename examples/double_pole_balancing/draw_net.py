84e2b59aaf00907d2655600132c4ded9657fcfe3
Adam Tupper <adam.tupper@outlook.com>
2020-04-20 14:12:49 +1200

    Remove accidentally copied line.

--- examples/double_pole_balancing/draw_net.py
+++ examples/double_pole_balancing/draw_net.py
@@ -2,5 +2,4 @@ import pickle
 from visualize import draw_net
 
 genome = pickle.load(open('results/run-0/solution.pickle', 'rb'))
-genome.mutate_add_node(None)
 draw_net(genome, filename='results/run-0/solution.gv')
